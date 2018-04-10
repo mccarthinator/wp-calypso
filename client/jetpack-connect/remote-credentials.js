4a9be186ff40e4583ad9d9336315d7f87bb77f6a
AnnaMag <anna.mag.kedzierska@gmail.com>
2018-04-10 13:25:23 +0100

    Display footer for manual installation only for notices retaining forms.
    Change retains the footer for non-error cases. Relevant conditional statements
    are simplified to account for both non-errors and errors with forms.

--- client/jetpack-connect/remote-credentials.js
+++ client/jetpack-connect/remote-credentials.js
@@ -244,7 +244,11 @@ export class OrgCredentialsForm extends Component {
 	}
 
 	footerLink() {
-		const { siteToConnect, translate } = this.props;
+		const { installError, siteToConnect, translate } = this.props;
+		const isFormInNotice = includes(
+			[ LOGIN_FAILURE, UNKNOWN_REMOTE_INSTALL_ERROR ],
+			this.getError( installError )
+		);
 		const manualInstallUrl = addQueryArgs(
 			{ url: siteToConnect },
 			'/jetpack/connect/instructions'
@@ -252,9 +256,11 @@ export class OrgCredentialsForm extends Component {
 
 		return (
 			<LoggedOutFormLinks>
-				<LoggedOutFormLinkItem href={ manualInstallUrl }>
-					{ translate( 'Install Jetpack manually' ) }
-				</LoggedOutFormLinkItem>
+				{ ( isFormInNotice || ! installError ) && (
+					<LoggedOutFormLinkItem href={ manualInstallUrl }>
+						{ translate( 'Install Jetpack manually' ) }
+					</LoggedOutFormLinkItem>
+				) }
 				<HelpButton />
 				<div className="jetpack-connect__navigation">
 					<Button
@@ -289,13 +295,13 @@ export class OrgCredentialsForm extends Component {
 
 		return (
 			<MainWrapper>
-				{ installError &&
-					! isFormInNotice && (
+				{ ! isFormInNotice &&
+					installError && (
 						<div className="jetpack-connect__notice">
 							<JetpackRemoteInstallNotices noticeType={ this.getError( installError ) } />
 						</div>
 					) }
-				{ ( ( installError && isFormInNotice ) || ! installError ) && (
+				{ ( isFormInNotice || ! installError ) && (
 					<div>
 						{ this.renderHeadersText() }
 						<Card className="jetpack-connect__site-url-input-container">
